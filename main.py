c082534576f03c098fdbec9d538cd966ed42551b
Andrey <a.karachyn@gmail.com>
2026-04-09 10:48:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,33 @@ app = FastAPI()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
+
 @app.post("/")
 async def webhook(request: Request):
     data = await request.json()
 
     if "message" in data:
         chat_id = data["message"]["chat"]["id"]
-        text = data["message"].get("text", "")
+        text = data["message"].get("text", "").lower()
 
-        if text.lower() == "заявка":
-            msg = "Оставьте заявку.\nНапишите услугу:\n1. СТУ\n2. Пожарные риски\n3. СПА\n4. КСОБ"
+        if text == "заявка":
+            msg = (
+                "Оставьте заявку.\n\n"
+                "Напишите услугу:\n"
+                "1. Разработка СТУ\n"
+                "2. Расчет пожарных рисков\n"
+                "3. Проектирование СПС, СОУЭ, Пожаротушение\n"
+                "4. Проектирование систем КСОБ"
+            )
         else:
             msg = "Напишите 'заявка' чтобы оставить запрос."
 
-        requests.post(URL, json={
-            "chat_id": chat_id,
-            "text": msg
-        })
+        requests.post(
+            URL,
+            json={
+                "chat_id": chat_id,
+                "text": msg
+            }
+        )
 
     return {"ok": True}
